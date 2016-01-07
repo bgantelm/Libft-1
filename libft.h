608c29acd33d4ff4bbeca965525285e2a0ba8cae
fhenri42 <fhenri.student@42.fr>
2016-01-07 17:13:18 +0000

    libft C9

--- libft.h
+++ libft.h
@@ -33,8 +33,7 @@ t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 void			ft_bzero(void *s, size_t n);
 void			*ft_memset(void *b, int c, size_t len);
 void			*ft_memcpy(void *dst, const void *src, size_t n);
-void			*ft_memccpy(void *restrict dst, const void *restrict src,
-					int c, size_t n);
+void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void			*ft_memmove(void *dst, const void *src, size_t len);
 void			*ft_memchr(const void *s, int c, size_t n);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
